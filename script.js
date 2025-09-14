26fc1c2d623a452ab5f17e333d86ca7e3a55e442
brandon <brandon@DESKTOP-9KX7J2Q.local>
2025-09-14 14:31:37 +0200

    Validació del nom d'usuari millorat

--- script.js
+++ script.js
@@ -1,58 +1,68 @@
 //setCookie("username", "", 1)
 //deleteCookie("username")
-window.addEventListener("load", ()=> {
-    const username = getCookie("username") || "";
-    if (username === "" || username.length > 50) {
-        console.log(`bad user ${username}`)
-        document.getElementById("changeUsername").style.display = "flex";
-    }
-    document.getElementById("applyUsername").addEventListener("click", () => {
-        const value = document.getElementById("username").value;
-        setCookie("username", value, 1);
-        document.getElementById("changeUsername").style.display = "none";
-        console.log(getCookie("username"))
-    })
+window.addEventListener("load", () => {
+  const username = getCookie("username") || "";
+  if (username === "" || username.length > 50) {
+    console.log(`bad user ${username}`);
+    document.getElementById("changeUsername").style.display = "flex";
+  } else {
     const welcome = document.getElementById("welcome");
     if (welcome) {
-        welcome.textContent = `Benvingut ${username}!`
+      welcome.textContent = `Benvingut ${username}!`;
     }
+  }
+  document.getElementById("applyUsername").addEventListener("click", () => {
+    const usernameInput = document.getElementById("username").value.trim();
+    if (usernameInput === "" || usernameInput.length > 50) {
+      alert("El nom s'uauri està buit o supera el limít de 50 caràscters");
+      return;
+    } else {
+      
+      const value = document.getElementById("username").value;
+      setCookie("username", value, 1);
+      const welcome = document.getElementById("welcome");
+      if (welcome) {
+        welcome.textContent = `Benvingut ${usernameInput}!`;
+      }
+      document.getElementById("changeUsername").style.display = "none";
+      console.log(getCookie("username"));
+    }
+  });
 });
 
 function setCookie(name, value, daysTolive) {
-    const date = new Date();
-    date.setTime(date.getTime() +  daysTolive *  24 * 60 * 60 * 1000); 
-    let expires = "expires=" +  date.toUTCString ();  
-    document.cookie = `${name}=${value}; ${expires };` 
+  const date = new Date();
+  date.setTime(date.getTime() + daysTolive * 24 * 60 * 60 * 1000);
+  let expires = "expires=" + date.toUTCString();
+  document.cookie = `${name}=${value}; ${expires};`;
 }
 
 function deleteCookie(name) {
-    setCookie(name, null, null);
+  setCookie(name, null, null);
 }
 
 function getCookie(name) {
-    const cDecoded =  decodeURIComponent(document.cookie);
-    const cArray = cDecoded.split("; ");
-    let result = null;
-    
-    cArray.forEach(element => {
-        if(element.indexOf(name)==0 ){
-             result = element.substring(name.length + 1)
-        }
-    })
-    return result;
+  const cDecoded = decodeURIComponent(document.cookie);
+  const cArray = cDecoded.split("; ");
+  let result = null;
+
+  cArray.forEach((element) => {
+    if (element.indexOf(name) == 0) {
+      result = element.substring(name.length + 1);
+    }
+  });
+  return result;
 }
 
 function showSection(sectionId) {
-    document.getElementById('menu').style.display = 'none';
-    document.getElementById('game').style.display = 'none';
-    document.getElementById('options').style.display = 'none';
-    if (sectionId === 'menu') {
-        document.getElementById('menu').style.display = 'flex';
-        var username = getCookie('username');
-        document.getElementById('welcome').textContent = `Benvingut ${username}`;
-    } else {
-        document.getElementById(sectionId).style.display = 'block';
-    }
-}  
-
-
+  document.getElementById("menu").style.display = "none";
+  document.getElementById("game").style.display = "none";
+  document.getElementById("options").style.display = "none";
+  if (sectionId === "menu") {
+    document.getElementById("menu").style.display = "flex";
+    var username = getCookie("username");
+    document.getElementById("welcome").textContent = `Benvingut ${username}`;
+  } else {
+    document.getElementById(sectionId).style.display = "block";
+  }
+}
